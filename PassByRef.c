1a425714ffaaecd74baf29291d7eb76bc94ee2c5
syam-cpu <meghasyam2002@gmail.com>
2026-09-08 00:24:56 +0530

    pointers intro pass by value and reference

---
+++ PassByRef.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void changeValue(int *x);
+
+int main(void) {
+    int a = 42;
+    changeValue(&a);
+    printf("Value of a after changeValue: %d\n", a);
+    return 0;
+}
+
+void changeValue(int *x) {
+    *x = 100; // This change will affect the original variable 'a'
+}
